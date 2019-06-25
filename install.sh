a3a214a1d9c4cefdc75ae3fc90bb6640c45be042
joserf <joserodriguesfilho@gmail.com>
2019-06-25 09:59:31 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ echo Interface de rede: $VNSTAT_REDE && sleep 2
 if [ -x /usr/bin/apt-get ]; then
 
   apt-get update && \
-  apt-get install vnstat zabbix-agent python-pip -y && \
+  apt-get install vnstat etherwake zabbix-agent python-pip -y && \
   pip install speedtest-cli
   sed -i "s/Server=127.0.0.1/Server=${ZABBIX_SERVER_IP}/" /etc/zabbix/zabbix_agentd.conf
   sed -i "s/ServerActive=127.0.0.1/ServerActive=${ZABBIX_SERVER_IP}/" /etc/zabbix/zabbix_agentd.conf
